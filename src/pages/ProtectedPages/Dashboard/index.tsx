c57ec2d4a12b1a46e3a483ebc938cd5126aef653
neg1t <n.fedchenko@solutionfactory.ru>
2024-08-23 02:22:22 +0700

    chore: страница планирования - добавлена возможность создавать план - необходимо задать имя пользователя перед планированием

--- src/pages/ProtectedPages/Dashboard/index.tsx
+++ src/pages/ProtectedPages/Dashboard/index.tsx
@@ -1,12 +1,44 @@
-import { Button, Flex, Typography } from 'antd'
+import { Button, Flex } from 'antd'
+import { useUnit } from 'effector-react'
+import { planModel } from 'entities/plan'
+import { userModel } from 'entities/user'
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { FIBONACCI_VOTES } from 'shared/lib/consts'
 
 export const ProtectedDashboardPage: React.FC = () => {
+  const navigate = useNavigate()
+
+  const { effects } = planModel
+
+  const isLoading = useUnit(effects.createPlanFx.pending)
+  const user = useUnit(userModel.stores.$user)
+
+  const createPlanHandler = () => {
+    if (user) {
+      effects
+        .createPlanFx({
+          creatorId: user.uid,
+          users: [
+            {
+              id: user.uid,
+              active: true,
+              name: user.displayName || '',
+            },
+          ],
+          votesToSelect: FIBONACCI_VOTES,
+        })
+        .then((uid) => {
+          navigate(`planning/${uid}`)
+        })
+    }
+  }
+
   return (
     <Flex vertical gap={20}>
-      <Typography.Title level={4}>Protected Dashboard Page</Typography.Title>
-      <Button type='primary'>Начать планирование</Button>
+      <Button type='primary' loading={isLoading} onClick={createPlanHandler}>
+        Начать планирование
+      </Button>
 
       <Outlet />
     </Flex>
--- src/pages/ProtectedPages/Planning/index.tsx
+++ src/pages/ProtectedPages/Planning/index.tsx
@@ -1,5 +1,136 @@
-import React from 'react'
+import React, { type ComponentProps, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { planModel, UsersList, VoteButtons } from 'entities/plan'
+import { useUnit } from 'effector-react'
+import { Button, Divider, Flex, Form, Input, Modal } from 'antd'
+import { Spinner } from 'shared/components'
+import { userModel } from 'entities/user'
+import { utils } from 'shared/utils'
+import { DB_TABLES } from 'shared/api'
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore'
+import type { Unsubscribe } from 'firebase/auth'
+import type { PlanDTO } from 'shared/api/plan/types'
+import './styles.scss'
 
 export const PlanningPage: React.FC = () => {
-  return <div>Planning Page</div>
+  const { id } = useParams<{ id: string }>()
+
+  const { effects, stores, events } = planModel
+
+  const plan = useUnit(stores.$currentPlan)
+  const mySelf = useUnit(userModel.stores.$user)
+
+  const isCreator = mySelf?.uid === plan?.creatorId
+  const voteButtonDisabled = !isCreator || false // todo проверка на всех проголосовавших
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe
+    if (id) {
+      unsubscribe = onSnapshot(
+        doc(getFirestore(), DB_TABLES.PLANNING, id!),
+        (doc) => {
+          events.planUpdate(doc.data() as PlanDTO)
+        },
+      )
+    }
+
+    return () => unsubscribe()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  // todo хорошо бы вынести логику с обновление имени пользователя в отдельных ху
+  const saveUserNameHandler = (values: { userName: string }) => {
+    if (mySelf) {
+      userModel.effects
+        .updateUserNameFx({
+          user: mySelf,
+          name: values.userName,
+        })
+        .then(() => {
+          effects.updatePlanUserNameFx({
+            userId: mySelf.uid,
+            planId: id!,
+            userName: values.userName,
+          })
+          Modal.destroyAll()
+        })
+    }
+  }
+
+  // todo хорошо бы вынести логику с обновление имени пользователя в отдельных хук
+  useEffect(() => {
+    if (mySelf && !mySelf.displayName) {
+      Modal.confirm({
+        maskClosable: false,
+        icon: null,
+        title: 'Кажется Вы не представились',
+        content: (
+          <Form layout='vertical' onFinish={saveUserNameHandler}>
+            <Form.Item
+              name='userName'
+              label='Имя'
+              required={false}
+              rules={[
+                {
+                  required: true,
+                  message: utils.validation.validationText.required,
+                },
+              ]}
+            >
+              <Input name='userName' placeholder='Введите имя' />
+            </Form.Item>
+            <Form.Item>
+              <Flex justify='space-between'>
+                <div />
+                <Button type='primary' htmlType='submit'>
+                  Сохранить
+                </Button>
+              </Flex>
+            </Form.Item>
+          </Form>
+        ),
+        footer: null,
+      })
+    } else {
+      Modal.destroyAll()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mySelf])
+
+  useEffect(() => {
+    if (id) {
+      effects.getPlanFx(id)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id])
+
+  const voteClickHandler: ComponentProps<typeof VoteButtons>['voteClick'] =
+    (vote) => () => {
+      console.log('vote', vote)
+    }
+
+  if (!plan) {
+    return <Spinner />
+  }
+
+  return (
+    <div className='planning-page'>
+      <Flex flex={'5%'} justify='center' className='planning-page__header'>
+        a
+      </Flex>
+      <Divider />
+      <Flex flex={'85%'} justify='center' className='planning-page__content'>
+        <Flex vertical gap={20}>
+          <UsersList />
+          <Button size='large' disabled={voteButtonDisabled} type='primary'>
+            Голосовать
+          </Button>
+        </Flex>
+      </Flex>
+      <Divider />
+      <Flex flex={'10%'} justify='center' className='planning-page__footer'>
+        <VoteButtons voteClick={voteClickHandler} />
+      </Flex>
+    </div>
+  )
 }
